16ff0dab4e2881105bec18552cfd9443de22d832
gnsdp99 <rladpgns99@gmail.com>
2025-06-10 11:42:54 +0900

    init project

---
+++ cicd-api/src/main/java/com/gnsdp/cicdapi/CicdApiApplication.java
@@ -0,0 +1,13 @@
+package com.gnsdp.cicdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdApiApplication.class, args);
+    }
+
+}
---
+++ cicd-api/src/test/java/com/gnsdp/cicdapi/CicdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnsdp.cicdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
