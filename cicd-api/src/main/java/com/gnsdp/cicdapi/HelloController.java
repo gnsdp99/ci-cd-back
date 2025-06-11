efcf00f7057b77656aaa34d2d1ea2bf2890f924e
gnsdp99 <rladpgns99@gmail.com>
2025-06-12 00:45:11 +0900

    컨트롤러를 수정한다

--- cicd-api/src/main/java/com/gnsdp/cicdapi/HelloController.java
+++ cicd-api/src/main/java/com/gnsdp/cicdapi/HelloController.java
@@ -1,12 +1,14 @@
 package com.gnsdp.cicdapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello!!!";
     }
